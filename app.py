5b7e398f8d46ea19996c6e9988719722cdde6aa2
Makoto Miyazaki <makoto.miyazaki1021@gmail.com>
2021-03-31 22:35:57 +0200

    added photos, changed map size

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import numpy
+import base64
+import os
 
 df = pd.read_csv("sample.csv")
 TOKEN = "pk.eyJ1IjoibWFrb3RvMTAyMSIsImEiOiJja213ZmZyenUwZWRxMnZwcTFzMWwzM2dmIn0.6idG-26PNC6pwiKqbYqiXQ"
@@ -21,10 +23,10 @@ fig = go.Figure(go.Scattermapbox(
         lon=df['lon'].values,
         mode='markers',
         marker=go.scattermapbox.Marker(
-            size=9, symbol="bus"
+            size=9, symbol="dog-park"
         ),
         text=df['name'],
-        customdata=df[['stars', 'id', 'comment']]
+        customdata=df[['stars', 'id']]
     ))
 
 fig.update_traces(
@@ -47,23 +49,32 @@ fig.update_layout(
         pitch=0,
         zoom=12
     ),
+    margin=dict(t=0, b=0, l=0, r=0)
 )
 
-app.layout = html.Div(
+app.layout = html.Div(className="app-header",
     children=[
-        html.H1(children='Find a pet-sitter nearby!'),
-        html.Div(dcc.Graph(figure=fig, id='map'), style={'width':'50%', 'display': 'inline-block'}),
-        html.Div(children=[
-            html.H3(id='name'), 
-            html.H4(id='stars'), 
-            html.H5(id='age'),
-            html.P(id='comment')], style={'width':'30%','display': 'inline-block'})
+        html.Div(html.H1(children='Find a pet-sitter nearby!'), className="app-header--title"),
+        html.Div(
+            dcc.Graph(figure=fig, id='map'), 
+            style={'width':'50%', 'display': 'inline-block'},
+            className="app-header--graph"),
+        html.Div(
+            className="app-header--desc",
+            children=[
+                html.Img(id='image', className="app-header--desc--image"),
+                html.H3(id='name'), 
+                html.H4(id='stars'), 
+                html.H5(id='age'),
+                html.P(id='comment')],
+                style={'width':'30%','display': 'inline-block'})
         
     ]
 )
 
 @app.callback(
     [
+        Output('image', 'src'),
         Output('name', 'children'),
         Output('age', 'children'),
         Output('comment', 'children'),
@@ -73,8 +84,9 @@ app.layout = html.Div(
         Input('map', 'clickData')
     ]
 )
-def plot_basin(data):
+def show_desc(data):
     if data is None:
+        image_url = ""
         name = ""
         age = ""
         comment = ""
@@ -85,7 +97,9 @@ def plot_basin(data):
         age = df[df['id']==id].age.values[0]
         comment = df[df['id']==id].comment.values[0]
         stars = df[df['id']==id].stars.values[0]
-    return name, age, comment, stars
+        image_url = "/assets/pictures/" + name + ".png"
+        print(image_url)
+    return image_url, name, age, comment, stars
 
 if __name__ == '__main__':
     app.run_server(
